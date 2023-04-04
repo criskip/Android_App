d71d1f6a33b6060d5f1b9c0c354308fd508f2ce6
eMobilis <preciousmwangi34@gmail.com>
2023-04-04 12:35:22 +0300

    Toasting messages on Android

--- app/src/main/java/com/example/chris_app/Login.kt
+++ app/src/main/java/com/example/chris_app/Login.kt
@@ -2,10 +2,29 @@ package com.example.chris_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Trace
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout.Tab
 
 class Login : AppCompatActivity() {
+private lateinit var email:EditText
+private lateinit var password:EditText
+private lateinit var loginBtn:Button
+private lateinit var regBtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        email = findViewById(R.id.edt_email)
+        password = findViewById(R.id.edt_password)
+        loginBtn = findViewById(R.id.login_Btn)
+        regBtn = findViewById(R.id.register_Btn)
+
+
+        loginBtn.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/chris_app/Toasting_android_Chris.kt
@@ -0,0 +1,30 @@
+package com.example.chris_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class Toasting_android_Chris : AppCompatActivity() {
+
+    private lateinit var displayinfo:TextView
+    private lateinit var user_edt:EditText
+    private lateinit var btn_sub:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toasting_android_chris)
+
+        displayinfo = findViewById(R.id.txtDisplay)
+        user_edt = findViewById(R.id.edtuserdata)
+        btn_sub = findViewById(R.id.btnsubmit)
+
+        //listen for events
+
+        btn_sub.setOnClickListener {
+            Toast.makeText(this, "I voted for Wajakoyah", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
