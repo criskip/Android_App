e6104b10b10163f96a4050999f3cddb8920e44ea
eMobilis <preciousmwangi34@gmail.com>
2023-04-13 12:11:20 +0300

    List Views

---
+++ app/src/main/java/com/example/chris_app/Simple_ListView_App.kt
@@ -0,0 +1,11 @@
+package com.example.chris_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Simple_ListView_App : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_list_view_app)
+    }
+}
